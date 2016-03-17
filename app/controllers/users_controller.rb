15ea290b88ae3e1a94fc7fffe6d5af634d8c2e23
bittu-moldedbits <bittu@moldedbits.com>
2016-03-17 18:11:46 +0530

    firebase server

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  def sign_in
+    if User.exists?(name: params[:name])
+      render :json=> {:success=>"success", :user_name=>"#{params[:name]}"}, :status=>200
+    else
+      render :json=> {:success=>"failure", :errors=>"user does not exits"}, :status=>401
+    end
+  end
+
+  def list_users
+    all_users = User.all.select(:name,:id)
+    render :json=> {:success=>"success", :user_array => all_users}, :status=>200
+  end
+
+  def start_chat
+
+  end
+end
