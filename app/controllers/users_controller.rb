b30f20b8f316c3b970e8c2882b9768b17f656d6e
bittu-moldedbits <bittu@moldedbits.com>
2016-03-17 19:43:29 +0530

    changed success parameter to boolean

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,15 @@
 class UsersController < ApplicationController
   def sign_in
     if User.exists?(name: params[:name])
-      render :json=> {:success=>"success", :user_name=>"#{params[:name]}"}, :status=>200
+      render :json=> {:success=>true, :user_name=>"#{params[:name]}"}, :status=>200
     else
-      render :json=> {:success=>"failure", :errors=>"user does not exits"}, :status=>401
+      render :json=> {:success=>false, :errors=>"user does not exits"}, :status=>401
     end
   end
 
   def list_users
     all_users = User.all.select(:name,:id)
-    render :json=> {:success=>"success", :user_array => all_users}, :status=>200
+    render :json=> {:success=>true, :user_array => all_users}, :status=>200
   end
 
   def start_chat
