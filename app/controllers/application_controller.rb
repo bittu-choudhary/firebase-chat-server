a6263958b2cb71c4ebc04bdd213359f69940083a
bittu-moldedbits <bittu@moldedbits.com>
2016-03-23 17:18:09 +0530

    config scheduler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,18 +10,26 @@ class ApplicationController < ActionController::Base
     app.save!
   end
   scheduler = Rufus::Scheduler.new
-  scheduler.every '5m' do
+  p "just after scheduler object"
+  scheduler.every '10s' do
     firebase = Firebase::Client.new(ENV['BASE_URI'],ENV['SECRET_KEY'])
+    p "im here"
     chats_object = firebase.get("chats")
+    p "chats_object"
+    p chats_object
     chats_object.body.each do |key, value|
       users = key.split("_",2)
       users.each do |user|
+          p "now im here with " + user
         if firebase.get("chats/" + key + "/" + user + "_unreadCount").body > 0
+          p "sending push"
           push_notification = Rpush::Gcm::Notification.new
           push_notification.app = Rpush::Gcm::App.find_by_name(ENV['RPUSH_APP_NAME'])
           message = "You have unread messages"
           title = "You've got a new message"
+          p "app"
           p push_notification.app
+          p "user"
           p User.find_by(name: user)
           push_notification.registration_ids = [User.find_by(name: user).device_id]
           push_notification.data = { title: title, message: message, user_name: user}
